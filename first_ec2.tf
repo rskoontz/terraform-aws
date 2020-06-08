a179b80f9c40fe4f900b494b4b75cb3dba1ac682
Bob Koontz <robertskoontz@gmail.com>
2020-06-08 11:59:11 -0700

    adding cross resource references

--- first_ec2.tf
+++ first_ec2.tf
@@ -5,11 +5,16 @@ provider "aws" {
   secret_key = var.secret-key
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "barebones-ec2" {
   ami = "ami-04e59c05167ea7bd5"
   instance_type = "t2.micro"
   subnet_id = "subnet-0d3153178b1dfee72"
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "vpc-0453b2a20efa68c8e"
+}
+
 
 resource "aws_eip" "a1" {
   vpc    = true
@@ -33,4 +38,9 @@ output "labs3bucket_domain" {
 
 output "labs3bucket_region" {
   value = aws_s3_bucket.lab-s3.region
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id    = aws_instance.barebones-ec2.id
+  allocation_id  = aws_eip.a1.id
 }
